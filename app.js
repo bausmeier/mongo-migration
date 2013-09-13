5bf34bb1d039fb2a26ada6b90d38b58640ecc99e
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-09-13 14:03:26 +0200

    Start implementing migration

--- app.js
+++ app.js
@@ -13,10 +13,16 @@ migrator.on('finish', function() {
 });
 
 var query = connection.query(
-  'SELECT posted_by.nme, posted_by.username, feed_post.* ' +
+  'SELECT ' +
+  ' posted_by.nme AS posted_by_name, posted_by.username AS posted_by_username, ' +
+  ' posted_for.nme AS posted_for_name, posted_for.username AS posted_for_username, ' +
+  ' feed_post.* ' +
   'FROM feed_post ' +
   'LEFT OUTER JOIN employee AS posted_by ' +
-  'ON feed_post.posted_by_employee_id = posted_by.id '
+  ' ON feed_post.posted_by_employee_id = posted_by.id ' +
+  'LEFT OUTER JOIN employee AS posted_for ' +
+  ' ON feed_post.posted_for_employee_id = posted_for.id '
+  //+ 'LIMIT 1 '
 );
 
 query.stream().pipe(migrator);
--- migrator.js
+++ migrator.js
@@ -3,6 +3,47 @@ var Writable = require('stream').Writable,
 var mongodb = require('mongodb'),
     MongoClient = mongodb.MongoClient;
 
+var convert = function(row) {
+  var doc = {};
+  // Copy fields
+  doc.id = row.id;
+  doc.tenant_id = row.tenant_id;
+  doc.message = row.message;
+  doc.date_created = row.date_created;
+  doc.date_updated = row.date_updated;
+  doc.type = row.post_type;
+  doc.feed_id = row.feed_id;
+  // Embed the posted by employee
+  if (row.posted_by_name || row.posted_by_username) {
+    doc.posted_by = {
+      name: row.posted_by_name,
+      username: row.posted_by_username
+    }
+  }
+  // Embed the posted for employee
+  if (row.posted_for_name || row.posted_for_username) {
+    doc.posted_for = {
+      name: row.posted_for_name,
+      username: row.posted_for_username
+    }
+  }
+  // Set parent if there is one
+  if (row.reply_to_feed_post_id) {
+    doc.parent_id = row.reply_to_feed_post_id;
+  }
+  // Handle type specific fields
+  switch (doc.type) {
+    case 1:
+      break;
+    default:
+      if (row.message_parameters) {
+        doc.parameters = row.message_parameters.split('|');
+      }
+      break;
+  }
+  return doc;
+};
+
 util.inherits(FeedPostMigrator, Writable);
 
 function FeedPostMigrator() {
@@ -25,13 +66,14 @@ FeedPostMigrator.prototype._write = function(chunk, encoding, done) {
 }
 
 FeedPostMigrator.prototype._migrate = function(chunk, done) {
-  if (chunk.reply_to_feed_post_id) {
-    this.collection.update({id: chunk.reply_to_feed_post_id}, {$push: {comments: chunk}}, function(err) {
+  var doc = convert(chunk);
+  if (doc.parent_id) {
+    this.collection.update({id: doc.parent_id}, {$push: {comments: doc}}, function(err) {
       if (err) throw err;
       done();
     });
   } else {
-    this.collection.insert(chunk, function(err) {
+    this.collection.insert(doc, function(err) {
       if (err) throw err;
       done();
     });
