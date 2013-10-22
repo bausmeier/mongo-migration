5bd86a88902aac19f6f276543830c5ef7b40fe11
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-10-22 11:14:54 +0200

    Wrap feed post migration in a promise

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql'),
-    FeedPostMigrator = require('./migrator');
+    FeedPostMigrator = require('./migrator'),
+    Q = require('q');
 
 // Connect to MySQL
 var connection = mysql.createConnection({
@@ -7,29 +8,54 @@ var connection = mysql.createConnection({
   database: 'bsg'
 });
 
-// Create a new migrator
-var migrator = new FeedPostMigrator({
-  database: 'mongodb://localhost/bsg'
-});
+function migrateFeedPosts() {
+  console.log('Migrating feed posts...');
+  var deferred = Q.defer();
 
-// Close the database connections when the finish event is fired
-migrator.on('finish', function() {
-  migrator.database.close();
-  connection.end();
-});
+  // Create a new migrator
+  var migrator = new FeedPostMigrator({
+    database: 'mongodb://localhost/bsg'
+  });
+  
+  // Close the database connections when the finish event is fired
+  migrator.on('finish', function() {
+    migrator.database.close();
+    connection.end();
+    deferred.resolve();
+  });
+  
+  migrator.on('error', function(err) {
+    deferred.reject(err);
+  });
+  
+  // Get all of the feed post info including the employees
+  var query = connection.query(
+    'SELECT ' +
+    ' posted_by.id AS posted_by_id, posted_by.nme AS posted_by_name, posted_by.username AS posted_by_username, ' +
+    ' posted_for.id AS posted_for_id, posted_for.nme AS posted_for_name, posted_for.username AS posted_for_username, ' +
+    ' feed_post.* ' +
+    'FROM feed_post ' +
+    'LEFT OUTER JOIN employee AS posted_by ' +
+    ' ON feed_post.posted_by_employee_id = posted_by.id ' +
+    'LEFT OUTER JOIN employee AS posted_for ' +
+    ' ON feed_post.posted_for_employee_id = posted_for.id ' +
+    'LIMIT 1'
+  );
+  
+  // Pipe the results of the query to the migrator
+  query.stream().pipe(migrator);
 
-// Get all of the feed post info including the employees
-var query = connection.query(
-  'SELECT ' +
-  ' posted_by.id AS posted_by_id, posted_by.nme AS posted_by_name, posted_by.username AS posted_by_username, ' +
-  ' posted_for.id AS posted_for_id, posted_for.nme AS posted_for_name, posted_for.username AS posted_for_username, ' +
-  ' feed_post.* ' +
-  'FROM feed_post ' +
-  'LEFT OUTER JOIN employee AS posted_by ' +
-  ' ON feed_post.posted_by_employee_id = posted_by.id ' +
-  'LEFT OUTER JOIN employee AS posted_for ' +
-  ' ON feed_post.posted_for_employee_id = posted_for.id '
-);
+  return deferred.promise;
+}
 
-// Pipe the results of the query to the migrator
-query.stream().pipe(migrator);
+function migrateLikes() {
+  console.log('Migrating likes...');
+  var deferred = Q.defer();
+  // TODO: Migrate likes
+  setTimeout(function() {
+    deferred.resolve();
+  }, 2000);
+  return deferred.promise;
+}
+
+migrateFeedPosts().then(migrateLikes).done();
