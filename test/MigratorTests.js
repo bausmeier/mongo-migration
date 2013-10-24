eae4064a72c95935ce5a8003be54915b2a16328e
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-10-24 20:09:15 +0200

    Add tests for migrator options

--- test/MigratorTests.js
+++ test/MigratorTests.js
@@ -1,7 +1,13 @@
 var FeedPostMigrator = require('../lib/FeedPostMigrator'),
     LikesMigrator = require('../lib/LikesMigrator'),
     expect = require('chai-for-sinon').expect,
-    Writable = require('stream').Writable;
+    Writable = require('stream').Writable,
+    Migrator = require('../lib/Migrator');
+
+var DEFAULT_DATABASE_URL = 'mongodb://localhost/test',
+    DEFAULT_COLLECTION = 'feedposts',
+    DATABASE_URL = 'mongodb://localhost/expected',
+    COLLECTION = 'expected';
 
 describe('FeedPostMigrator', function() {
   
@@ -10,13 +16,31 @@ describe('FeedPostMigrator', function() {
     it('should return a new instance', function() {
       var migrator = new FeedPostMigrator();
       expect(migrator).to.be.an.instanceOf(FeedPostMigrator);
+      expect(migrator).to.be.an.instanceOf(Migrator);
       expect(migrator).to.be.an.instanceOf(Writable);
     });
     
     it('should return a new instance without the new keyword', function() {
       var migrator = FeedPostMigrator();
       expect(migrator).to.be.an.instanceOf(FeedPostMigrator);
+      expect(migrator).to.be.an.instanceOf(Migrator);
       expect(migrator).to.be.an.instanceOf(Writable);
+    });
+
+    it('should have default options', function() {
+      var migrator = new FeedPostMigrator();
+      expect(migrator.db).to.equal(DEFAULT_DATABASE_URL);
+      expect(migrator.col).to.equal(DEFAULT_COLLECTION);
+    });
+
+    it('should accept options', function() {
+      var options = {
+        database: DATABASE_URL,
+        collection: COLLECTION
+      };
+      var migrator = new FeedPostMigrator(options);
+      expect(migrator.db).to.equal(DATABASE_URL);
+      expect(migrator.col).to.equal(COLLECTION);
     });
   });
 });
@@ -28,13 +52,31 @@ describe('LikesMigrator', function() {
     it('should return a new instance', function() {
       var migrator = new LikesMigrator();
       expect(migrator).to.be.an.instanceOf(LikesMigrator);
+      expect(migrator).to.be.an.instanceOf(Migrator);
       expect(migrator).to.be.an.instanceOf(Writable);
     });
     
     it('should return a new instance without the new keyword', function() {
       var migrator = LikesMigrator();
       expect(migrator).to.be.an.instanceOf(LikesMigrator);
+      expect(migrator).to.be.an.instanceOf(Migrator);
       expect(migrator).to.be.an.instanceOf(Writable);
+    });
+
+    it('should have default options', function() {
+      var migrator = new LikesMigrator();
+      expect(migrator.db).to.equal(DEFAULT_DATABASE_URL);
+      expect(migrator.col).to.equal(DEFAULT_COLLECTION);
+    });
+
+    it('should accept options', function() {
+      var options = {
+        database: DATABASE_URL,
+        collection: COLLECTION
+      };
+      var migrator = new LikesMigrator(options);
+      expect(migrator.db).to.equal(DATABASE_URL);
+      expect(migrator.col).to.equal(COLLECTION);
     });
   });
 });
